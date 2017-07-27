308fd36419a04bc5c67ffd6a2652e985e359128a
João Neto <joaoneto@anid.com.br>
2017-07-27 17:29:54 -0300

    Add o que eu esqueci

---
+++ src/pages/serviceFavoritos.ts
@@ -0,0 +1,40 @@
+import {Injectable} from "@angular/core";
+import { Storage } from '@ionic/storage';
+
+@Injectable()
+export class ServiceFavoritos {
+  public favs;
+
+  constructor(public storage: Storage) {
+    this.favs = [];
+    this.atualizaFavoritos();
+  }
+
+  atualizaFavoritos() {
+    this.storage.get('favs').then((data) => {
+        this.favs = JSON.parse(data) || [];
+    }, (error) => {
+        console.log("ERROR: " + JSON.stringify(error));
+    });
+  }
+
+  getFavoritos() {
+    return this.favs;
+  }
+
+  cadastraFavorito(idAgenda) {
+    if( this.favs.indexOf(idAgenda) == -1 ) {
+      this.favs.push(idAgenda);
+      this.storage.set('favs', JSON.stringify(this.favs));
+    }
+  }
+
+  removeFavorito(idAgenda) {
+    this.favs = this.favs.filter((fav) => {
+      console.info('fav', fav);
+      console.info('id_agenda', idAgenda);
+      return fav != idAgenda;
+    }, idAgenda);
+    this.storage.set('favs', JSON.stringify(this.favs));
+  }
+}
